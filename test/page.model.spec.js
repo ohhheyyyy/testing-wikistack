d9fdd3a59a6c54df6df4701e54aec860eaa72112
ohhheyyyy <ashley.hartt@gmail.com>
2016-05-05 16:50:13 -0400

    Testing wikistack workshop completed through instance methods

--- test/page.model.spec.js
+++ test/page.model.spec.js
@@ -1,5 +1,8 @@
 var expect = require('chai').expect;
 var chai = require('chai');
+var Promise = require('bluebird');
+chai.should();
+chai.use(require('chai-things'));
 var spies = require('chai-spies');
 chai.use(spies);
 
@@ -50,13 +53,6 @@ describe('Page model', function() {
                 })
                 .catch(done);
         });
-        afterEach(function(done) {
-            Page.sync({ force: true })
-                .then(function() {
-                    done();
-                })
-                .catch(done);
-        });
         describe('findByTag', function() {
             it('gets pages with the search tag', function(done) {
                 Page.findByTag('bar')
@@ -77,48 +73,95 @@ describe('Page model', function() {
         });
     });
 
+    describe('Instance methods', function() {
+        var page1;
+        var page2;
+        var page3;
+        beforeEach(function(done) {
+            Promise.all([
+                    Page.create({
+                        title: 'page1',
+                        content: 'page1 content',
+                        tags: ['help', 'me']
+                    }),
 
-describe('Instance methods', function() {
-    describe('findSimilar', function() {
-        it('never gets itself');
-        it('gets other pages with any common tags');
-        it('does not get other pages without any common tags');
+                    Page.create({
+                        title: 'page2',
+                        content: 'page2 content',
+                        tags: ['thank', 'me']
+                    }),
+
+                    Page.create({
+                        title: 'page3',
+                        content: 'page3 content',
+                        tags: ['cool']
+                    })
+                ]).spread(function(p1, p2, p3) {
+                    page1 = p1;
+                    page2 = p2;
+                    page3 = p3;
+                    done();
+                })
+                .catch(done);
+        });
+        afterEach(function(done) {
+            Page.sync({ force: true })
+                .then(function() {
+                    done();
+                })
+                .catch(done);
+        });
+
+        describe('findSimilar', function() {
+            it('never gets itself', function(done) {
+                page1.findSimilar()
+                    .then(function(foundPages) {
+                        expect(foundPages[0].dataValues.title).to.not.equal("page1");
+                        done();
+                    })
+                    .catch(done);
+            });
+        });
+        it('gets other pages with any common tags', function(done) {
+            page1.findSimilar()
+                .then(function(foundPages) {
+                    expect(foundPages).to.have.deep.property("[0].dataValues.title", "page2");
+                    done();
+                })
+                .catch(done);
+        });
+        it('does not get other pages without any common tags', function(done) {
+            page3.findSimilar()
+                .then(function(foundPages) {
+                    expect(foundPages).to.have.length(0);
+                    done();
+                });
+        });
     });
+
+    describe('Validations', function() {
+        var page;
+        beforeEach(function() {
+            page = new Page();
+        });
+        it('errors without title', function(done) {
+            page.validate()
+                .then(function(err) {
+                    expect(err).to.exist;
+                    expect(err.errors).to.exist;
+                    expect(err.errors[0].path).to.equal('title');
+                    done();
+                });
+        });
+        it('errors without content');
+        it('errors given an invalid status');
+    });
+
+    describe('Hooks', function() {
+        it('it sets urlTitle based on title before validating');
+    });
+
+
+
 });
 
-describe('Validations', function() {
-    it('errors without title');
-    it('errors without content');
-    it('errors given an invalid status');
-});
-
-describe('Hooks', function() {
-it('it sets urlTitle based on title before validating');
-});
-
-});
-
-
-
-
-
-// describe("Page.title", function() {
-//     it("return", function() {
-//         expect(2 + 2).to.be.equal(4);
-//     });
-// });
-// describe("Page.urlTitle", function() {
-
-// });
-// describe("Page.content", function() {
-
-// });
-// describe("Page.status", function() {
-
-// });
-// describe("Page.date", function() {
-
-// });
-// describe("Page.tags", function() {
-
-// });
